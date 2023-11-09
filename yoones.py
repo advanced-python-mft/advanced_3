c1de8a5b4a12c5aa81d26f5b15c51e18e55c4078
yoones <yoonesakbari3@gmail.com>
2023-11-09 16:21:32 +0330

    finish2

--- yoones.py
+++ yoones.py
@@ -1,19 +1,19 @@
 from datetime import datetime
 notebook=[]
-def add_book(note):
+def add_note(note):
     note{"titel":input("title:"),
-     "note":input("note"),
-     "date":datetime.now()}
+      "note":input("note"),
+      "date":datetime.now()}
     notebook.append(note)
     return notebook
-    while True:
-        command = input("""
+while True:
+     command = input("""
 chikar konam?
 a=append note
 e=exit
 show=shownote""")
      if command = "a":
-         notebook = add_book(notebook)
+         notebook = add_note(notebook)
      elif command = "e"
          break
      elif command = "show"
