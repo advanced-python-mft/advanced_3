2b2fa5103c169ce87e0047ef13fb08d6db4f2581
Morteza <maleki.morteza92@gmail.com>
2023-11-09 16:44:45 +0330

    corrected the code from syntax err

--- yoones.py
+++ yoones.py
@@ -1,20 +1,24 @@
 from datetime import datetime
-notebook=[]
+
+notebook = []
+
 def add_book(note):
-    note{"titel":input("title:"),
+    note = {"titel":input("title:"),
      "note":input("note"),
      "date":datetime.now()}
     notebook.append(note)
     return notebook
-    while True:
-        command = input("""
+
+while True:
+    command = input("""
 chikar konam?
 a=append note
 e=exit
 show=shownote""")
-     if command = "a":
-         notebook = add_book(notebook)
-     elif command = "e"
-         break
-     elif command = "show"
-          print(notebook)
+    
+    if command == "a":
+        notebook = add_book(notebook)
+    elif command == "e":
+        break
+    elif command == "show":
+        print(notebook)
