adef12a30d73e67a518c6b893f0ca644ad6355ec
yoones <yoonesakbari3@gmail.com>
2023-11-09 16:05:27 +0330

    add time

--- yoones.py
+++ yoones.py
@@ -2,5 +2,5 @@ from datetime import datetime
 note=[]
 def add_book(note):
     note{"titel":input("title:"),
-   "note":input("note")
-   "date":datetime.now()}
+     "note":input("note"),
+     "date":datetime.now()}
